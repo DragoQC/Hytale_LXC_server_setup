c8420fac41b846956c9c0c0eada210c0b29e9832
DragoQC <alexisgauthier2001@hotmail.com>
2026-01-14 13:27:22 -0500

    added -y

--- install-server.sh
+++ install-server.sh
@@ -6,7 +6,7 @@ apt install -y wget apt-transport-https gpg
 wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
 echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
 apt update
-apt install temurin-25-jdk temurin-25-jre
+apt install temurin-25-jdk temurin-25-jre -y
 #CLI folder
 mkdir -p /opt/hytale_server/cli
 #Downloading CLI
